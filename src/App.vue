0a722559ec624a4b58c22a53d86bf09cf8874d6c
hitcherland <hitcherland@gmail.com>
2019-05-29 22:34:44 +0100

    simple drawing implemented, along with naive undo/redo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,121 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div @keydown.ctrl.z="undo"
+         @keydown.ctrl.r.prevent="redo">
+        <vue-headful title="drawpg" description="drawing rpg platform"/>
+        <v-stage tabindex=0 :config="configKonva"
+                 @mousedown="startDrawing"
+                 @mousemove="keepDrawing"
+                 @mouseup="stopDrawing">
+            <v-layer>
+                <v-circle :config="configCircle"></v-circle>
+                <v-path v-for="item in paths" :key="item.id" :config="item"></v-path>
+            </v-layer>
+        </v-stage>
+        <div class="top-right">
+            <swatches @input="setLineColor" v-model="lineColor" colors="text-advanced" popover-to="left"></swatches>
+            <swatches @input="setFillColor" v-model="fillColor" colors="text-advanced" popover-to="left"></swatches>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    data() {
+        return {
+            is_drawing: false,
+            active_path: undefined,
+            paths: [],
+            undos: [],
+            lineColor: '#000000',
+            fillColor: '#ffffff',
+            configKonva: {
+                width: 200,
+                height: 200
+            },
+            configCircle: {
+                x: 100,
+                y: 100,
+                radius: 10,
+                fill: "#ffffff",
+                stroke: "#000000",
+                strokeWidth: 4
+            },
+        };
+    },
+    created() {
+        window.addEventListener('resize', this.handleResize)
+        this.handleResize();
+    },
+    methods: {
+        handleResize() {
+            this.configKonva.width = window.innerWidth;
+            this.configKonva.height = window.innerHeight;
+        },
+        setLineColor(color) {
+            this.configCircle.stroke = color;
+        },
+        setFillColor(color) {
+            this.configCircle.fill = color;
+        },
+        startDrawing(e) {
+            this.is_drawing=true;
+            this.configCircle.x = e.evt.pageX;
+            this.configCircle.y = e.evt.pageY;
+            this.activePath = {
+                x: e.evt.pageX, y: e.evt.pageY,
+                data: 'M0,0',
+                fill: this.fillColor,
+                stroke: this.lineColor,
+                __initial__: [e.evt.pageX, e.evt.pageY]
+            };
+            this.undos = [];
+            this.paths.push(this.activePath);
+        },
+        keepDrawing(e) {
+            if(!this.is_drawing)
+                return;
+            this.configCircle.x = e.evt.pageX;
+            this.configCircle.y = e.evt.pageY;
+            this.activePath.data += 'L' + (e.evt.pageX - this.activePath.__initial__[0])+ ',' + (e.evt.pageY - this.activePath.__initial__[1])
+        },
+        stopDrawing(e) {
+            this.is_drawing=false;
+            this.configCircle.x = e.evt.pageX;
+            this.configCircle.y = e.evt.pageY;
+            this.activePath.data += 'L' + (e.evt.pageX - this.activePath.__initial__[0])+ ',' + (e.evt.pageY - this.activePath.__initial__[1]) + 'z'
+        },
+        undo() {
+            if(this.paths.length > 0) {
+                this.undos.push(this.paths.pop())
+            }
+        },
+        redo() {
+            if(this.undos.length > 0) {
+                this.paths.push(this.undos.pop())
+            }
+        }
+    }
+};
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+    margin: 0
+}
+
+.top-right {
+    position:absolute;
+    top: 0;
+    right: 0;
+    background: rgba(0, 0, 0, 0.5);
+    padding: 0.5rem;
+    border-radius: 0 0 0 0.5rem;
+}
+
+.top-right > * {
+    display: inline-block;
+    margin: 0.25rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
+import VueKonva from 'vue-konva'
+import vueHeadful from 'vue-headful'
+import Swatches from 'vue-swatches'
+import "vue-swatches/dist/vue-swatches.min.css"
 import App from './App.vue'
 
+Vue.use(VueKonva)
+Vue.component('vue-headful', vueHeadful)
+Vue.component('Swatches', Swatches)
 Vue.config.productionTip = false
 
 new Vue({
